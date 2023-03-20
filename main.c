2ad0f9f139dc32422c2ef1b10cdd67aceaa65011
dyl-baw <dvd.y.lee@gmail.com>
2023-03-20 15:52:24 -0700

    restructured my server code to be more concise and smaller functions

--- main.c
+++ main.c
@@ -1,18 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <getopt.h>
 
-int main(int argc, char *argv[]) {
-    int opt;
-    char *ip_address = NULL;
-    int port = 0;
-    int client_running = 1;
+#define MAX_CONNECTIONS 5
+#define DEFAULT_PORT 80
 
-    while ((opt = getopt(argc, argv, "i:p:")) != -1) {
+int socket_fd;
+void createSocket()
+{
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1 ) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void bindSocket(int server_fd, const char *ip_address, int port)
+{
+    struct sockaddr_in address;
+    memset(&address, 0 , sizeof(address));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(ip_address);
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) == - 1)
+    {
+        perror("Binding socket failed.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void listenSocket(int server_fd)
+{
+    if ( listen(socket_fd, MAX_CONNECTIONS) == -1)
+    {
+        perror("Listening failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void acceptConnection(int server_fd)
+{
+    int client_fd;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_length = sizeof(client_addr);
+
+    client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_length);
+    if (client_fd == -1)
+    {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void run_server(int argc, char *argv[])
+{
+    char *ip_address = NULL;
+    int port = DEFAULT_PORT;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "i:p:")) != -1)
+    {
         switch (opt) {
             case 'i':
                 ip_address = optarg;
@@ -21,58 +74,28 @@ int main(int argc, char *argv[]) {
                 port = atoi(optarg);
                 break;
             default:
-                fprintf(stderr, "Usage: %s -i <IP address> -p <port number>\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-i ip address ] [-p port] \n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
-
-    if (ip_address == NULL || port == 0) {
-        fprintf(stderr, "Usage: %s -i <IP address> -p <port number>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in serv_addr;
-    int sock_fd = 0;
-    int message_read;
-    char buffer[1024];
-    char message[1024];
-
-    // Create a socket
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("\n Socket creation error \n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up the server address structure
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    // Connect to the server
-    if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
-        printf("\nConnection Failed \n");
-        exit(EXIT_FAILURE);
-    } else {
-        printf("Connected to Server.\n\n");
-    }
-
-    while (client_running)
+    //if no port is provided, use the default port of 80.
+    if (port == 0)
     {
-        memset(message, 0, sizeof(message)); // clear the message buffer
-        printf("Please enter a message: ");
-        fgets(message, 1024, stdin);
-        // Send the message to the server
-        send(sock_fd, message, strlen(message), 0);
-        printf("\nMessage sent\n");
-
-        memset(message, 0, sizeof(message)); // clear the message buffer
-
-        // Receive a response from the server
-        message_read = read(sock_fd, buffer, 1024);
-        printf("\n%s\n", buffer);
-        memset(buffer, 0, sizeof(buffer)); // clear the buffer
-        memset(message, 0, sizeof(message)); // clear the message buffer
+        port = DEFAULT_PORT;
     }
-    // Close the socket
-    close(sock_fd);
+
+    createSocket();
+    bindSocket(socket_fd,ip_address, port);
+    listenSocket(socket_fd);
+
+    printf("Server running on IP:%s PORT:%d\n", ip_address, port);
+
+    acceptConnection(socket_fd);
+}
+
+int main(int argc, char *argv[])
+{
+    run_server(argc, argv);
+
     return EXIT_SUCCESS;
 }
