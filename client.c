9c33ca088380d2bb78450ece0c9bf8310dbf31b6
dyl-baw <dvd.y.lee@gmail.com>
2023-04-05 02:02:21 -0700

    Made some functions to make it more atomic.

--- client.c
+++ client.c
@@ -1,112 +1,97 @@
 #include <arpa/inet.h>
-#include <fcntl.h>
-#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 4096
+#define DEFAULT_PORT 80
 
-int main(int argc, char *argv[]) {
-    if (argc < 6) {
-        printf("Usage: %s <HEADER|GET|POST> <IP_ADDRESS> <SERVER_PORT> <file_path|-> <endpoint> <custom_header>\n", argv[0]);
-        return 1;
-    }
 
-    int sockfd;
-    struct sockaddr_in server_addr;
-    struct hostent *host;
+void send_request(int sock, const char *method, const char *path, const char *server) {
     char buffer[BUFFER_SIZE];
-    char *method = argv[1];
-    char *server_address = argv[2];
-    int server_port = atoi(argv[3]);
-    char *file_path = argv[4];
-    char *endpoint = argv[5];
-    char *custom_header = argv[6];
 
-    if (strcmp(method, "GET") != 0 && strcmp(method, "POST") != 0 && strcmp(method, "HEADER") != 0) {
-        printf("Error: Invalid method. Use HEADER, GET, or POST.\n");
-        return 1;
-    }
+    if (strcmp(method, "GET") == 0 || strcmp(method, "HEAD") == 0)
+        {
+        snprintf(buffer, BUFFER_SIZE,
+                 "%s %s HTTP/1.0\r\n"
+                 "Server: %s\r\n"
+                 "User-Agent: Custom Client\r\n"
+                 "Accept: */*\r\n"
+                 "\r\n",
+                 method, path, server);
+        send(sock, buffer, strlen(buffer), 0);
 
-    host = gethostbyname(server_address);
-    if (host == NULL) {
-        perror("Error: Unable to get host by name.");
-        return 1;
-    }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Error: Unable to create socket.");
-        return 1;
-    }
-
-    bzero((char *)&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
-    memcpy(&server_addr.sin_addr, host->h_addr, host->h_length);
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error: Unable to connect.");
-        return 1;
-    }
-
-    if (strcmp(method, "POST") == 0) {
-        int file_fd = open(file_path, O_RDONLY);
-        if (file_fd < 0) {
-            perror("Error: Unable to open file.");
-            return 1;
+        while (recv(sock, buffer, BUFFER_SIZE - 1, 0) > 0)
+        {
+            printf("%s", buffer);
+        }
+    } else if (strcmp(method, "POST") == 0)
+        {
+        FILE *file = fopen(path, "rb");
+        if (!file) {
+            perror("Error opening file");
+            exit(EXIT_FAILURE);
         }
 
-        struct stat file_stat;
-        fstat(file_fd, &file_stat);
+        fseek(file, 0, SEEK_END);
+        size_t file_size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        char *file_contents = malloc(file_size);
+        fread(file_contents, 1, file_size, file);
+        fclose(file);
 
         snprintf(buffer, BUFFER_SIZE,
                  "POST %s HTTP/1.0\r\n"
-                 "Content-Length: %ld\r\n"
+                 "Server: %s\r\n"
+                 "User-Agent: Custom Client\r\n"
                  "Content-Type: application/octet-stream\r\n"
-                 "%s\r\n"
+                 "Content-Length: %zu\r\n"
                  "\r\n",
-                 endpoint, file_stat.st_size, custom_header);
+                 path, server, file_size);
+        send(sock, buffer, strlen(buffer), 0);
+        send(sock, file_contents, file_size, 0);
 
-        send(sockfd, buffer, strlen(buffer), 0);
+        free(file_contents);
+    }
+}
 
-        ssize_t bytes_read;
-        while ((bytes_read = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
-            send(sockfd, buffer, bytes_read, 0);
-        }
 
-        close(file_fd);
-    } else if (strcmp(method, "GET") == 0) {
-        snprintf(buffer, BUFFER_SIZE,
-                 "GET %s HTTP/1.0\r\n"
-                 "%s\r\n"
-                 "\r\n",
-                 endpoint, custom_header);
-
-        send(sockfd, buffer, strlen(buffer), 0);
-    } else
-    {
-        snprintf(buffer, BUFFER_SIZE,
-                 "HEADER %s HTTP/1.0\r\n"
-                 "%s\r\n"
-                 "\r\n",
-                 endpoint, custom_header);
-
-        send(sockfd, buffer, strlen(buffer), 0);
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [GET|HEAD|POST] <path> [Server IP] [port]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    ssize_t bytes_received = recv(sockfd, buffer, BUFFER_SIZE -1, 0);
-    if (bytes_received > 0)
+    const char *method = argv[1];
+    const char *path = argc > 2 ? argv[2] : "/"; // if argc is greater than 2, set path to 2. Otherwise it is /
+    const char *server_ip = argc > 3 ? argv[3] : "127.0.0.1"; // if argc is greater than 3, if so, set server_ip to argv[3]
+    int port = argc > 4 ? atoi(argv[4]) : DEFAULT_PORT; // if argc is > than we set the integer value of port to argv[4], otherwise it is set to DEFAULT_PORT
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
     {
-        buffer[bytes_received] = '\0';
-        printf("Server Response: \n %s \n ", buffer);
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
     }
 
-    close(sockfd);
-    return(EXIT_SUCCESS);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error connecting to server");
+        exit(EXIT_FAILURE);
+    }
+
+    send_request(sock, method, path, server_ip);
+
+    close(sock);
+    return 0;
 }
